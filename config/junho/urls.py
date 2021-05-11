98f3c5a96ae9e53ef5150291e5c4a6ca376960a6
Tarakyu <worrasong@gmail.com>
2021-05-11 17:36:35 +0900

    make rooms

--- config/junho/urls.py
+++ config/junho/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 import junho.views
 
 urlpatterns = [
-    path('main/', junho.views.mainpage, name = 'mainpage'),
+    path('room1/', junho.views.room1, name = 'room1'),
+    path('room2/', junho.views.room2, name = 'room2'),
+    path('room3/', junho.views.room3, name = 'room3'),
 ]
